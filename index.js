98beb4ae4197dc67c9bd4fb6de03d9d06f41f372
nehalalwani123 <111523029+nehalalwani123@users.noreply.github.com>
2023-07-06 09:04:34 +0530

    initial

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from 'cors'
+import bodyParser from "body-parser";
+import env from 'dotenv'
+import { Configuration, OpenAIApi } from "openai";
+
+const app = express()
+
+env.config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const configuration = new Configuration({
+    organization: "org-OBjR9IX3fzYHBB4evVWpxbJU",
+    apiKey: process.env.API_KEY
+})
+const openai = new OpenAIApi(configuration)
+
+app.listen("3080", () => {
+    console.log("listening on port 3080")
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello world!!")
+})
+
+app.post('/', async (req, res) => {
+    const { message } = req.body
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${message}`,
+            max_tokens: 100,
+            temperature: .5
+        })
+        res.json({ message: response.data.choices[0].text })
+    } catch (e) {
+        console.log(e)
+        res.send(e).status(400)
+    }
+})
